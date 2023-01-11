e35ba17fe703ef97442e42e5bc5be210f3e2ba97
susanagandra <susana.gandra@mindswap.academy>
2023-01-11 16:00:00 +0000

    teste de commit

--- Restaurant/src/Client.java
+++ Restaurant/src/Client.java
@@ -1,2 +1,33 @@
 public class Client {
+    private int tableNumber;
+    public Client clientNumber;
+
+    public Client() {
+       this.clientNumber = new Client();
+
+
+    }
+
+    public static int askForTable(int tableNumber){
+        
+        return 0;
+    }
+
+    public static int order(){
+        return 0;
+    }
+
+    public static boolean pay(){
+        boolean pay = false;
+
+    }
+
+    public static void setRestaurant() {
+
+    }
+
+    private static void checkRestaurantPresence(){
+
+    }
+
 }
