0f47e6510c673ef8b70bc7bb0daa4f8e5d60c17a
fgoncalves <filipa.i.goncalves@gmail.com>
2023-01-11 17:15:53 +0000

    alterações ao Restaurant.java

--- Restaurant/src/Main.java
+++ Restaurant/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        Client client1 = new Client();
+        Client client2 = new Client();
+        Client client3 = new Client();
+        Client client4 = new Client();
+        Client client5 = new Client();
+
+        Client[][] clients  = {{client1, client2}, {client3, client4, client5}};
+
+
+        Restaurant restaurant = new Restaurant("Comesses em casa!","Imaginary Food", clients);
         System.out.println("Hello world!");
     }
 }
--- Restaurant/src/Restaurant.java
+++ Restaurant/src/Restaurant.java
@@ -1,2 +1,62 @@
 public class Restaurant {
+
+    private Client[][] c;
+    private Table[] tables;
+    private boolean fullCapacity;
+    private int numberOfOccupiedTables;
+    private int RestaurantCount;
+
+    private String name;
+    private String kindOfFood;
+
+    public Restaurant(String name, String kindOfFood, Client[][] clients){
+
+        this.name = name;
+        this.kindOfFood = kindOfFood;
+
+        Table table1 = new Table(2);
+        Table table2 = new Table(4);
+
+
+        Table[] existingTables= {table1, table2};
+        this.tables = existingTables;
+        this.c = clients;
+    }
+
+    public void findTable(){
+
+        for (int i = 0; i < this.tables.length; i++){
+
+            if (this.tables[i].isFree == true) {
+                if(this.tables[i].seats <= c[i].length) {
+                    numberOfOccupiedTables++;
+                    occupyTable(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean freeTable(){
+
+
+        return true;
+    }
+
+    public int getRestaurantCount() {
+        return 0;
+    }
+
+    public void receiveOrder() {
+
+    }
+
+    private boolean checkIfTableCanBeFree() {
+        Rtes
+        return true;
+    }
+
+    private void occupyTable(int numberOfTable){
+        this.tables[numberOfTable].isFree = false;
+    }
 }
