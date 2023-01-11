a8e3f4efae6d32d7fbedee8f51fe5311987ac750
KaffaJaffa <rafael.lopes@mindswap.academy>
2023-01-11 17:15:02 +0000

    Mesas

--- Restaurant/src/Table.java
+++ Restaurant/src/Table.java
@@ -1,2 +1,40 @@
 public class Table {
+
+    private final int seats;
+    private boolean isFree;
+    private String order;
+
+public Table (int numberOfSeats){
+    this.seats = numberOfSeats;
+    this.isFree = true;
+    this.order = "";
+}
+    public static int cleanOrder(){
+        return 0;
+    }
+
+    public static void getOrder(){
+
+    }
+    public void setOrder(String order){
+        this.order = order;
+    }
+
+    public static boolean isFree(){
+        return false;
+    }
+
+
+    public void occupy(){
+        this.isFree = false;
+    }
+
+    public static void saveOrder(){
+
+    }
+
+    public void unoccupy(){
+
+        this.isFree = true;
+    }
 }
